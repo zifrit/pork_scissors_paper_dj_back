be7009a47021a4acf2ce901977e578e0701457f3
zifrit <zifrit@list.ru>
2024-03-02 14:00:57 +0300

    init project rock_scissor_paper__dj_back

---
+++ rsp/my_user_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MyUserAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'my_user_app'
---
+++ rsp/rsp/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.conf import settings
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+if settings.DEBUG:
+    urlpatterns += [
+        path('__debug__/', include('debug_toolbar.urls')),
+    ]
